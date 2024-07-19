797546ef5618628ae307779ce22693749bf0080e
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-07-19 17:07:43 -0400

    Fixes logout GET issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,16 +2,23 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import "../stylesheets/application"
 import "@popperjs/core"
 import { Tooltip, Toast, Popover } from 'bootstrap'
 
 import Rails from "@rails/ujs"
 Rails.start()
 
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true
+
+document.addEventListener('turbo:load', function () {
+  console.log('Turbo Loaded');
+});
+
 document.addEventListener("turbo:load", () => {
-  // Initialize tooltips, popovers, etc.
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new Tooltip(tooltipTriggerEl)
   })
-})
+});
